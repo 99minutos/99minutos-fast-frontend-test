3847e6a3f7ccabd42e41139fe502692c369c30a8
sadotsoy <sadotcortes@gmail.com>
2021-06-22 12:29:51 -0500

    :art: pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Containers from "../components/Containers";
 
 export default function Home() {
   return (
     <div className={styles.mainTitle}>
-      <h1>99minutos Fast Frontend Test</h1>
+      <Containers />
     </div>
   );
 }
